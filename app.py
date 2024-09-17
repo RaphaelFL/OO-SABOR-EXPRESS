1d62790e9d291874b7bb937b01d690bfcddc82ba
RaphaelFL <52725627+RaphaelFL@users.noreply.github.com>
2024-09-17 10:40:56 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from model.restaurante import Restaurante
+
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
+restaurante_pizza = Restaurante('Japa', 'Japones')
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Lais', 8)
+restaurante_praca.receber_avaliacao('Emy', 5)
+
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
